8e3a858a154721b829e01e5a07d0d6b7a27a2f25
fujeh <davidrhoads34@gmail.com>
2019-06-02 22:39:27 -0400

    second commit

--- force-app/main/default/lwc/masterAccounts/masterAccounts.js
+++ force-app/main/default/lwc/masterAccounts/masterAccounts.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getMasterList from '@salesforce/apex/AccountController.getMasterList';
+import MASTER_ACCOUNT_FIELD from '@salesforce/schema/Account.Master_Account_Number__c';
 
-export default class MasterAccounts extends LightningElement {}
+export default class MasterAccounts extends LightningElement {
+    @wire(getMasterList, { recordId: '$recordId', fields: [MASTER_ACCOUNT_FIELD] }) account;
+}
